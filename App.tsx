5cc7e52260e669e02bbdbf4405ba1b73ac04a9e7
Kimssmin <chakane5@naver.com>
2023-09-04 23:32:29 +0900

    Chapter 12 completed

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import RootStack from './screens/RootStack';
+import qs from 'qs';
 
 function App() {
   return (
     <NavigationContainer>
+      <RootStack />
     </NavigationContainer>
   );
 }
--- screens/RootStack.tsx
+++ screens/RootStack.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import {Button, Text, View} from 'react-native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { useNavigation } from '@react-navigation/native';
+import { Button, Text, View } from 'react-native';
+import { NativeStackNavigationProp, createNativeStackNavigator } from '@react-navigation/native-stack';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
+import MainTab, { MainTabNavigationScreenParams } from './MainTab';
 
-const Stack = createNativeStackNavigator();
+type RootStackParamList = {
+  MainTab: undefined;
+  Detail: {
+    id: number;
+  };
+};
 
-function HomeScreen() {
-	const navigation = useNavigation();
-	const onPress = () => {
-		navigation.navigate('Detail');
-	};
-	return (
-		<View>
-			<Text>Home</Text>
-			<Button title="Open Detail" onPress={onPress} />
-		</View>
-	);
-}
+export type RootStackNavigationProp =
+  NativeStackNavigationProp<RootStackParamList>;
 
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+type DetailScreenRouteProp = RouteProp<RootStackParamList, 'Detail'>;
 function DetailScreen() {
+  const { params } = useRoute<DetailScreenRouteProp>();
   return (
     <View>
-      <Text>Detail</Text>
+      <Text>Detail {params.id}</Text>
     </View>
   );
 }
@@ -29,7 +29,9 @@ function DetailScreen() {
 function RootStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen component={HomeScreen} name='Home' />
+      <Stack.Screen component={MainTab} name='MainTab' options={{
+        headerShown: false,
+      }} />
       <Stack.Screen component={DetailScreen} name="Detail" />
     </Stack.Navigator>
   );
